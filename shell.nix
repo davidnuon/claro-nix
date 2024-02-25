e3d83fa234ca41fc00419bd17524092ebfebaedb
David Nuon <davidnuongm@gmail.com>
2024-02-25 00:20:00 +0000

    Change to non-flake example

---
+++ shell.nix
@@ -0,0 +1,24 @@
+{...}:
+let 
+  unstableTarball =
+    fetchTarball
+       https://github.com/NixOS/nixpkgs/archive/master.tar.gz;
+
+  pkgs = import unstableTarball {};
+in
+(pkgs.buildFHSUserEnv {
+  name = "bazel-userenv-example";
+  targetPkgs = pkgs: [
+    # Use Bazelisk to manage Bazel install
+    pkgs.bazelisk
+
+    # Bazel depdencies
+    pkgs.glibc
+    pkgs.gcc
+
+    # Claro Java Depdencies
+    pkgs.zlib.dev
+    pkgs.zlib.out
+  ];
+})
+.env
